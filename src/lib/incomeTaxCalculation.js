7b19b148e56157b125dbd3f4ebdf1e320155ccb8
alistairgray <alistair.g.gray@gmail.com>
2021-06-28 15:37:17 +1000

    completed initial logic for marginal income tax calculation

---
+++ src/lib/incomeTaxCalculation.js
@@ -0,0 +1,80 @@
+// **************************************** //
+// Hardcoded Data
+// **************************************** //
+
+const employeeInfo = [
+
+    {
+        id: 457613,
+        firstName: 'David', 
+        lastName: 'Rudd', 
+        annualSalary: 60050, 
+        super: 0.09,
+        paymentStartDate: 2020-03-01,
+        paymentEndDate: 2020-03-31
+    },
+    {
+        id: 258612,
+        firstName: 'Ryan', 
+        lastName: 'Chen', 
+        annualSalary: 120000, 
+        super: 0.10,
+        paymentStartDate: 2020-03-01,
+        paymentEndDate: 2020-03-31
+    }
+] // employeeInfo[]
+
+// PSEUDOCODE //
+// Create an array of objects to hold the employee information
+// This information will be referred to by other functions
+
+function incomeTaxCalc(input){
+    
+    const data = input
+
+    // Arrays have the structure of:
+    // Min, Max, Rate, Additional Payment
+    const taxBrackets = [
+        {min: 0, max: 18200, rate: 1, fee: 0},
+        {min: 18201, max: 37000, rate: 0.19, fee: 0},
+        {min: 37001, max: 80000, rate: 0.325, fee: 3572},
+        {min: 180000, max: 99999999, rate: 0.45, fee: 54547}
+    ]; // taxBrackets[]
+
+    data.forEach(({firstName:name, annualSalary:salary}) => {
+
+        let taxToPay = 0
+
+        console.log('*****************************');
+        console.log(`${name}'s Salary is $${salary}`);
+        console.log('*****************************');
+
+        // for...of loop necessary:
+        // a forEach will continue one extra step after a return
+        // I have used a for...of to utilise a break which avoids this
+
+        // checks if salary fits within a tax bracket
+        for(let bracket of taxBrackets) {
+            if(salary < bracket.max) {
+                console.log(bracket);
+                let taxToPay = ((salary - bracket.min)*bracket.rate) + bracket.fee;
+
+                // rounds up value
+                taxToPay = Math.ceil(taxToPay)
+                console.log('*****************************');
+                console.log(`${name}'s income tax balance is $${taxToPay}`);
+                console.log('-----------------------------');
+                break;
+
+                // otherwise it adds to a running total of 
+                // marginal tax
+            } else {
+                console.log(bracket);
+                taxToPay += ((bracket.max - bracket.min)*bracket.rate) + bracket.fee;
+                console.log(`Running Total: ${taxToPay}`);
+            }
+        } // for...of taxBrackets
+    }); // forEach data
+} // incomeTaxCalc()
+
+incomeTaxCalc(employeeInfo);
---
+++ src/lib/thinking.js
@@ -0,0 +1,11 @@
+// ------------------------------------------------------------------------------- //
+// THINKING 
+// At a foundational level, this is a problem of navigating and interacting with objects.
+// The goal will be to initially prove the base requirements via vanilla javascript and
+// then make it work in HTML and CSS. 
+// The design needs to be a blend between pleasant visuals and function.
+// Testing wise, the project will need to be integrated into React via create-react-app, with
+// Jest to verify.
+// A nice to have would be to have the ability to adjust the payment months 
+
+// 
