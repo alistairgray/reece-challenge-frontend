f2f559b9dcfa2fff77adc423d0347f24f13b9491
alistairgray <alistair.g.gray@gmail.com>
2021-07-04 14:52:19 +1000

    debugging testing suite. added enzyme and babel config files

--- src/components/_tests_/PayslipApp.test.js
+++ src/components/_tests_/PayslipApp.test.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import { render, screen, cleanup } from '@testing-library/react'
-
 import PayslipApp from '../PayslipApp';
-// Utilising Jest to create initial app foundations
 
-describe('Document Testing', () => {
-
-    test('should render PayslipTable component', () => {
-        render(<PayslipTable />);
-        const PayslipTableElement = screen.getAllByTestId('PayslipTable');
-        expect(PayslipTableElement).toBeInTheDocument();
-    }) // render the PayslipTable component
-
-    test('render the h1 of PayslipApp', () => {
-        const {getByText} = render(<PayslipApp />);
-        const linkElement = getByText('Your Payslip');
-        expect(linkElement).toBeInTheDocument();
-    }); // render the title of app
-
-}); // document testing
+describe('Addition', () => {
+    it('knows that 2 and 2 make 4', () => {
+      expect(2 + 2).toBe(4);
+    });
+  });
