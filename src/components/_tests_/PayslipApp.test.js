6098a6046a9a19abafd2cf210192e8536ff365ba
alistairgray <alistair.g.gray@gmail.com>
2021-07-04 17:34:57 +1000

    added fireevent test for new employee form

--- src/components/_tests_/PayslipApp.test.js
+++ src/components/_tests_/PayslipApp.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PayslipApp from '../PayslipApp';
 
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 
 describe('Addition', () => {
     it('knows that 2 and 2 make 4', () => {
@@ -20,6 +20,21 @@ describe('PayslipApp', () => {
         render(<PayslipApp />)
         const element = screen.getByText('Monthly Payslip Breakdown')
         expect(element).toBeTruthy();
+    });
+
+    // testing the form here since it relies on prop functions from this
+    // component
+    it('should render the form for adding a new employee', () => {
+      render(<PayslipApp />);
+      fireEvent.change(screen.getByLabelText(/First Name/i),     { target: { value: 'testFirst' } });
+      fireEvent.change(screen.getByLabelText(/Last Name/i),      { target: { value: 'testLast'  } });
+      fireEvent.change(screen.getByLabelText(/Annual Salary/i),  { target: { value: '120000' } });
+      fireEvent.change(screen.getByLabelText(/Superannuation/i), { target: { value: '10'     } });
+      fireEvent(
+        screen.getByText('submit'),
+        new MouseEvent('click', { bubbles: true, cancelable: true, })
+      );
+      const newRow = screen.getByText('testFirst');
     });
 })
 
